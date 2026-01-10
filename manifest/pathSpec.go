86cbb08bc356fad9fe36f738029bd3ac5593f129
Kid A <thisismeamir@outlook.com>
2026-01-10 11:32:16 +0330

    [delete]

--- manifest/pathSpec.go
+++
@@ -1,9 +0,0 @@
-package manifest
-
-// PatchSpec defines a patch to apply to the source
-type PatchSpec struct {
-	URL      string `yaml:"url,omitempty"`
-	Path     string `yaml:"path,omitempty"` // local path in index repo
-	Checksum string `yaml:"checksum,omitempty"`
-	Level    int    `yaml:"level,omitempty"` // patch -p level
-}
