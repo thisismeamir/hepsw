15d89f56ae9c8e79856ba077195a3791aa244aa2
Kid A <thisismeamir@outlook.com>
2026-01-11 11:58:45 +0330

    [implementation] struct model of HepSw configuration.

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -1,23 +1,52 @@
 package configuration
 
 type Configuration struct {
-	Workspace     string `yaml:"workspace"`
-	SourcesDir    string `yaml:"sourcesDir"`
-	BuildsDir     string `yaml:"buildsDir"`
-	InstallsDir   string `yaml:"installsDir"`
-	EnvsDir       string `yaml:"envsDir"`
-	LogsDir       string `yaml:"logsDir"`
-	ToolchainsDir string `yaml:"toolchainsDir"`
-	indexDir      string `yaml:"indexDir"`
+	Workspace  string `yaml:"workspace"`
+	Sources    string `yaml:"sources"`
+	Builds     string `yaml:"builds"`
+	Installs   string `yaml:"installs"`
+	Envs       string `yaml:"envs"`
+	Logs       string `yaml:"logs"`
+	Toolchains string `yaml:"toolchains"`
+	Index      string `yaml:"index"`
+	Thirdparty string `yaml:"thirdparty"`
 
 	State      WorkspaceState `yaml:"state"`
 	UserConfig UserConfig     `yaml:"userConfig"`
 }
 
 type WorkspaceState struct {
-	Packages     map[string]interface{} `yaml:"packages"`
-	Sources      map[string]interface{} `yaml:"sources"`
-	Environments map[string]interface{} `yaml:"environments"`
+	Packages     []WorkspacePackageState     `yaml:"packages"`
+	Sources      []WorkspaceSourceState      `yaml:"sources"`
+	Environments []WorkspaceEnvironmentState `yaml:"environments"`
+}
+
+type WorkspacePackageState struct {
+	PackageId   string   `yaml:"packageId"`
+	Name        string   `yaml:"name"`
+	Path        string   `yaml:"path"`
+	Version     string   `yaml:"version"`
+	BuildTime   string   `yaml:"buildTime"`
+	InstallTime string   `yaml:"installTime"`
+	IsUsedBy    []string `yaml:"isUsedBy"`
+	IsUsing     []string `yaml:"isUsing"`
+}
+
+type WorkspaceSourceState struct {
+	SourceId string   `yaml:"sourceId"`
+	Name     string   `yaml:"name"`
+	Path     string   `yaml:"path"`
+	Version  string   `yaml:"version"`
+	IsUsedBy []string `yaml:"isUsedBy"`
+	IsUsing  []string `yaml:"isUsing"`
+}
+
+type WorkspaceEnvironmentState struct {
+	EnvironmentId string   `yaml:"environmentId"`
+	Name          string   `yaml:"name"`
+	Description   string   `yaml:"description"`
+	ScriptPath    string   `yaml:"scriptPath"`
+	Packages      []string `yaml:"packages"`
 }
 
 type UserConfig struct {
