510c2af9a4f662fe376532d762a5c09e65d4bed2
Kid A <thisismeamir@outlook.com>
2026-01-11 12:00:10 +0330

    [implementation] Configuration health check without checking the state and user configs is done.

--- internal/configuration/configHealth.go
+++ internal/configuration/configHealth.go
@@ -1,6 +1,7 @@
 package configuration
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 
@@ -8,9 +9,10 @@ import (
 	"github.com/thisismeamir/hepsw/internal/utils"
 )
 
-type ConfigurationHealthType interface {
-}
-
+// ConfigHealth
+// Checks the configuration file health by checking that the directories exist, as well as
+// checking the state of the workspace, packages installed, environments and other important
+// stuff.
 func ConfigHealth() error {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
@@ -22,21 +24,24 @@ func ConfigHealth() error {
 	if _, err := os.Stat(hepswConfigPath); os.IsNotExist(err) {
 		return err
 	}
-	viper.SetConfigName("hepsw")
-	viper.AddConfigPath(hepswConfigPath)
-	viper.SetConfigType("yaml")
-	if err := viper.ReadInConfig(); err != nil {
+	newConfig := viper.New()
+	newConfig.SetConfigType("yaml")
+	newConfig.SetConfigName("hepsw")
+	newConfig.AddConfigPath("/home/kid-a/.hepsw")
+
+	if err := newConfig.ReadInConfig(); err != nil {
 		return err
 	}
 
 	var config Configuration
-	if err := viper.Unmarshal(&config); err != nil {
-		return err
+	unmarshallingError := newConfig.Unmarshal(&config)
+	if unmarshallingError != nil {
+		return unmarshallingError
 	}
 
-	directoryError := config.CheckDirectories()
+	info, directoryError := config.CheckDirectories()
 	if directoryError != nil {
-		return directoryError
+		return errors.New(directoryError.Error() + " named " + info)
 	}
 
 	stateError := config.CheckState()
@@ -52,33 +57,36 @@ func ConfigHealth() error {
 	return nil
 }
 
-func (config *Configuration) CheckDirectories() error {
+func (config *Configuration) CheckDirectories() (string, error) {
 	// Checking that all the directories in the workspace exist
 	if _, err := utils.CheckDirectory(config.Workspace); err != nil {
-		return err
+		return "workspace", err
 	}
-	if _, err := utils.CheckDirectory(config.SourcesDir); err != nil {
-		return err
+	if _, err := utils.CheckDirectory(config.Sources); err != nil {
+		return "sources", err
 	}
-	if _, err := utils.CheckDirectory(config.BuildsDir); err != nil {
-		return err
+	if _, err := utils.CheckDirectory(config.Builds); err != nil {
+		return "builds", err
 	}
-	if _, err := utils.CheckDirectory(config.InstallsDir); err != nil {
-		return err
+	if _, err := utils.CheckDirectory(config.Installs); err != nil {
+		return "installs", err
 	}
-	if _, err := utils.CheckDirectory(config.EnvsDir); err != nil {
-		return err
+	if _, err := utils.CheckDirectory(config.Envs); err != nil {
+		return "envs", err
 	}
-	if _, err := utils.CheckDirectory(config.LogsDir); err != nil {
-		return err
+	if _, err := utils.CheckDirectory(config.Logs); err != nil {
+		return "logs", err
 	}
-	if _, err := utils.CheckDirectory(config.ToolchainsDir); err != nil {
-		return err
+	if _, err := utils.CheckDirectory(config.Toolchains); err != nil {
+		return "toolchains", err
 	}
-	if _, err := utils.CheckDirectory(config.indexDir); err != nil {
-		return err
+	if _, err := utils.CheckDirectory(config.Index); err != nil {
+		return "index", err
 	}
-	return nil
+	if _, err := utils.CheckDirectory(config.Thirdparty); err != nil {
+		return "thirdparty", err
+	}
+	return "", nil
 }
 
 func (config *Configuration) CheckState() error {
