e254a7ebf039057bd31463e0a9bdaac8d92e5518
Kid A <thisismeamir@outlook.com>
2026-02-16 10:04:22 +0330

    [implementation]
    More structures implemented for packages, such as combined package with versions, combined versions with dependencies and more.

--- internal/index/package.go
+++ internal/index/package.go
@@ -39,3 +39,37 @@ type Dependency struct {
 	Optional            bool
 	Condition           *string
 }
+
+type PackageWithVersions struct {
+	Package  *Package
+	Versions []*Version
+}
+
+type VersionWithDependencies struct {
+	Version      *Version
+	Dependencies []*Dependency
+}
+
+type PackageInfo struct {
+	ID            int64
+	Name          string
+	Description   string
+	VersionCount  int
+	LatestRelease string
+}
+
+type LatestVersion struct {
+	Name        string
+	Description string
+	Version     *Version
+	ManifestURL string
+	PublishedAt time.Time
+}
+
+type ReverseDependency struct {
+	DependencyName     string
+	DependentPackageID *int64
+	DependencyVersion  *int64
+	VersionConstraint  string
+	Optional           bool
+}
