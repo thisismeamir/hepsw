d34a798233c639a9cb1232248e31e08abe603eff
Kid A <thisismeamir@outlook.com>
2026-02-19 19:09:19 +0330

    [refactorization]

--- internal/index/models/searchIdentity.go
+++ internal/index/models/searchIdentity.go
@@ -5,12 +5,12 @@ import (
 	"strings"
 )
 
-type searchPackageIdentity struct {
+type SearchPackageIdentity struct {
 	Name    string
 	Version string
 }
 
-func GetSearchPackageIdentity(input string) (*searchPackageIdentity, error) {
+func GetSearchPackageIdentity(input string) (*SearchPackageIdentity, error) {
 	// Split the input into name and version
 	parts := strings.SplitN(input, ":", 2)
 	if len(parts) == 0 {
@@ -23,7 +23,7 @@ func GetSearchPackageIdentity(input string) (*searchPackageIdentity, error) {
 		version = parts[1]
 	}
 
-	return &searchPackageIdentity{
+	return &SearchPackageIdentity{
 		Name:    name,
 		Version: version,
 	}, nil
